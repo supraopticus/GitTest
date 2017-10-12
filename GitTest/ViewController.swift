c95b2b536eb0195d0196a9c403907c5599ef4b8a
Rob Rabi <robrabi@gmail.com>
2017-10-12 18:12:56 -0400

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Rob Rabi on 10/12/17.
+//  Copyright © 2017 Rob Rabi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
